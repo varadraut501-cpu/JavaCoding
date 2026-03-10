aa4a9c3b8170c8b5ffd02e3be991f778f44524f1
varadraut501 <varadraut501@gmail.com>
2026-03-10 14:56:32 +0530

    Condition

---
+++ Condition.java
@@ -0,0 +1,25 @@
+package JavaCoding;
+
+import java.util.*;
+
+
+ class Condition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a == b) {
+            System.out.println("Equal");
+        } else {
+            if(a > b) {
+                System.out.println("a is greater");
+            } else {
+                System.out.println("a is lesser");
+
+            sc.close();
+
+            }
+        }
+    }
+}

8ffb03fcf6185917e55798cb788f19f5aeebae1b
varadraut501 <varadraut501@gmail.com>
2026-03-10 14:17:18 +0530

    Sum

---
+++ Sum.java
@@ -0,0 +1,14 @@
+package JavaCoding;
+
+    public class Sum {
+public static void main(String[] args) {
+    // varibles
+    int a = 10;
+    int b = 25;
+    int sum = a + b;
+    
+   System.out.print(sum);
+
+       }
+}
+

476f57763861156931a6c684412b066610cc2134
varadraut501 <varadraut501@gmail.com>
2026-03-11 22:13:11 +0530

    PositiveNegative

---
+++ PositiveNegative.java
@@ -0,0 +1,24 @@
+package JavaCoding;
+
+import java.util.Scanner;
+
+public class PositiveNegative {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        
+        // Checking condition
+        if (num > 0) {
+            System.out.println(num + " is Positive");
+        } else if (num < 0) {
+            System.out.println(num + " is Negative");
+        } else {
+            System.out.println("The number is Zero");
+        }
+        
+        sc.close();
+    }
+}

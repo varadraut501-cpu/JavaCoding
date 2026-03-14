de86905cec8c28502b42c436852bb2702d03ff1a
varadraut501 <varadraut501@gmail.com>
2026-03-14 21:48:20 +0530

    LargestElement

---
+++ LargestElement.java
@@ -0,0 +1,34 @@
+package JavaCoding;
+
+import java.util.Scanner;
+public class LargestElement {
+    public static void main(String[] args) {
+
+Scanner sc = new Scanner(System.in);
+
+System.out.print("enter number of elements : ");
+
+int n = sc.nextInt();
+int[] arr = new int[n];        
+
+System.out.println("enter " + n + "elements : ");
+
+for(int i = 0; i < n; i++) {
+arr[i] = sc.nextInt();
+    }
+
+
+int largest = arr[0];
+
+for(int i = 1; i<n; i++){
+if(arr[i] > largest) {
+largest = arr[i];
+}
+
+}
+
+System.out.println("Largest element is : " + largest);
+sc.close();
+
+}
+}

a987b59ee3ff15d169598cc3e05c93ced94ffd15
varadraut501 <varadraut501@gmail.com>
2026-03-15 15:56:52 +0530

    MethodDemo

---
+++ MethodDemo.java
@@ -0,0 +1,18 @@
+package JavaCoding;
+
+public class MethodDemo {
+
+    // Method definition
+    void greet() {
+        System.out.println("Hello, welcome to Java!");
+    }
+
+    public static void main(String[] args) {
+        
+        // Creating object
+        MethodDemo obj = new MethodDemo();
+        
+        // Method calling
+        obj.greet();
+    }
+}

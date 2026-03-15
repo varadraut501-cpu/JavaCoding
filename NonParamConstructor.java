b12cd4a7be2503fd2547c700c3401cf953c7da4d
varadraut501 <varadraut501@gmail.com>
2026-03-15 15:59:28 +0530

    NonParamConstructor

---
+++ NonParamConstructor.java
@@ -0,0 +1,28 @@
+package JavaCoding;
+
+import java.util.Scanner;
+
+class Student {
+    int rollNo;
+    String name;
+
+    // Non-Parameterized Constructor
+    Student() {
+        rollNo = 100;
+        name = "Varad";
+        System.out.println("Constructor is called");
+    }
+
+    void display() {
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Name: " + name);
+    }
+}
+
+public class NonParamConstructor {
+    public static void main(String[] args) {
+
+        Student s1 = new Student();   // Constructor automatically called
+        s1.display();
+    }
+}

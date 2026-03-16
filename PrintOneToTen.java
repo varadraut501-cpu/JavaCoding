ced68edd799205e7cde98387af1595c5285393df
varadraut501 <varadraut501@gmail.com>
2026-03-16 18:05:17 +0530

    PrintOneToTen

---
+++ PrintOneToTen.java
@@ -0,0 +1,9 @@
+package JavaCoding;
+
+public class PrintOneToTen {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}

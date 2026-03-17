fd87a7eba4e7307322b6b02a58e4f51aef5de201
varadraut501 <varadraut501@gmail.com>
2026-03-17 16:33:47 +0530

    SearchElement

---
+++ SearchElement.java
@@ -0,0 +1,40 @@
+package JavaCoding;
+
+import java.util.Scanner;
+
+public class SearchElement {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter element to search: ");
+        int key = sc.nextInt();
+
+        boolean found = false;
+
+        // Linear Search
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == key) {
+                System.out.println("Element found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+
+        if(!found) {
+            System.out.println("Element not found in array.");
+        }
+
+        sc.close();
+    }
+}

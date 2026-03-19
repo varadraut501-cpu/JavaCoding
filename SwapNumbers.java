d753e9277689fac2fb5be8e2b7c39e1d1fee2d75
varadraut501 <varadraut501@gmail.com>
2026-03-19 11:33:20 +0530

    SwapNumbers

---
+++ SwapNumbers.java
@@ -0,0 +1,27 @@
+package JavaCoding;
+
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        
+        // Swapping without third variable
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        
+        System.out.println("After swapping:");
+        System.out.println("First number: " + num1);
+        System.out.println("Second number: " + num2);
+        
+        sc.close();
+    }
+}
